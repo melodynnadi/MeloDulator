8d9e56a7c8d4ff78932acc110d92d088c7e65c18
melodynnadi <melodynnadi2@gmail.com>
2025-03-01 20:09:51 -0600

    Update script.js

--- Calculator App/script.js
+++ Calculator App/script.js
@@ -25,17 +25,4 @@ function findPercentage(){
     display.value = display.value / 100;
 }
 
-const titleText = "Melodulator";
-let index = 0;
-const titleElement = document.getElementById("title");
 
-function typeTitle() {
-    if (index < titleText.length) {
-        titleElement.textContent += titleText.charAt(index);
-        index++;
-        setTimeout(typeTitle, 150); // Adjust speed as needed
-    }
-}
-
-// Start the animation when page loads
-window.addEventListener('load', typeTitle);
